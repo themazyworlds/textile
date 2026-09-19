ea019ce7c8be9e797a7ee6f31b132ad99743f9f1
themazyworlds <meerahemant9086@gmail.com>
2026-09-19 16:39:10 +0530

    chore: set package release version to 0.1.0

--- src/textile/__init__.py
+++ src/textile/__init__.py
@@ -7,4 +7,4 @@ import warnings
 # Suppress upstream library deprecation warnings from typing internals on Python 3.14+
 warnings.filterwarnings("ignore", category=DeprecationWarning, message=".*_UnionGenericAlias.*")
 
-__version__ = "0.1.2"
+__version__ = "0.1.0"
